8627fa15030088ac97cc6ea2e9c118fdb8426837
m.dadaev <m.dadaev@ceteralabs.com>
2015-02-15 01:52:41 +0300

    1 - complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
-(function() {
+(function () {
     var app = angular.module('gemStore', []);
+    var gems = [
+        {
+            name: 'Azurite',
+            price: 2.95,
+            canPurchase: false,
+            soldOut: false
+        },
+        {
+            name: 'Bloodstone',
+            price: 2.95,
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Zircon',
+            price: 5,
+            canPurchase: true,
+            soldOut: false
+        }
+
+    ];
+
+    app.controller("StoreController", function () {
+        this.products = gems;
+    })
+
 })();
