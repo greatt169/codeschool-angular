f0ad615c69435f7cdfe4d52a1cf58545658e7187
mikhail <dadaev@aeroidea.ru>
2021-09-12 08:34:15 +0300

    push

--- go/code/chan-gen.go
+++ go/code/chan-gen.go
@@ -1,20 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"math/rand"
 	"time"
 )
 
+func genOneRandom(generated chan int, dict map[int]int) {
+
+}
+
 func uniqRandn(n int) []int {
-	rand.Seed(time.Now().UnixNano())
 	result := make([]int, n)
 	dict := make(map[int]int)
-	for i := 0; i < n; i++ {
+	i := 0
+	for i != n {
+		rand.Seed(time.Now().UnixNano())
 		x := rand.Int()
-		if _, ok := dict[x]; ok == false {
+		if _, ok := dict[x]; !ok {
 			result[i] = x
 			dict[x]++
+			i++
 		} else {
 			continue
 		}
@@ -23,5 +29,8 @@ func uniqRandn(n int) []int {
 }
 
 func main() {
-	fmt.Println(uniqRandn(10))
+	start := time.Now()
+	uniqRandn(10)
+	elapsed := time.Since(start)
+	log.Printf("uniqRandn took %s", elapsed)
 }
--- sql/test-tasks.md
+++ sql/test-tasks.md
@@ -14,20 +14,21 @@ CREATE TABLE public.pets (
     id integer,
     animal character varying(255)
 );
-
-COPY public.pets (id, animal) FROM stdin;
-1	кошка
-2	кошка
-2	собака
-3	пони
-4	питон
-4	мышь
-4	хомяк
-\.
+-----------------
+user_id | animal
+-----------------
+1	    | кошка
+2	    | кошка
+2	    | собака
+3	    | пони
+4	    | питон
+4	    | мышь
+4	    | хомяк
+------------------
 ```
 
 #### Задача
-Вывести всех суотрудников, у которых нет кошек
+Вывести всех пользователей, у которых нет кошек
 ```
 select distinct t1.id from pets as t1 left join pets as t2 on t1.id = t2.id and t2.animal = 'кошка' where t2.id is null
 ```
